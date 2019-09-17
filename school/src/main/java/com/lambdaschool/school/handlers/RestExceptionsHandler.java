8810a81f5ff8da88e378a9b3ec1fa90188b00cd4
JCRN <clemons.jason@gmail.com>
2019-09-16 18:56:48 -0700

    added exception handling for noHandlerFound, enabled WebMVC, created application context

--- school/src/main/java/com/lambdaschool/school/handlers/RestExceptionsHandler.java
+++ school/src/main/java/com/lambdaschool/school/handlers/RestExceptionsHandler.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
@@ -50,6 +52,29 @@ public class RestExceptionsHandler extends ResponseEntityExceptionHandler
     @Override
     protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request)
     {
-        
+        ErrorDetail errorDetail = new ErrorDetail();
+
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle(ex.getMethod());
+        errorDetail.setDetail(request.getDescription(true));
+        errorDetail.setDeveloperMessage("HTTP Method Not Valid for Endpoint (check for valid URI and proper HTTP " +
+                "Method)");
+
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request)
+    {
+        ErrorDetail errorDetail = new ErrorDetail();
+
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle(ex.getRequestURL());
+        errorDetail.setDetail(request.getDescription(true));
+        errorDetail.setDeveloperMessage("Rest Handler Not Found (check for valid URI)");
+
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
     }
 }
